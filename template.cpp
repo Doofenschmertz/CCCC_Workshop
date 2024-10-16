2cd3fb6ec3a13fde6c29632ee0883d17c55ee9e2
Doofenschmertz <f20230508@pilani.bits-pilani.ac.in>
2024-10-16 22:26:11 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+
+void solve(){
+
+    
+}
+
+int32_t main() {
+    // fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
